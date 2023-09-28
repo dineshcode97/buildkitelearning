e2a9eeab7c514bf87d4df801e8ed5397580bda17
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2023-09-28 16:02:00 +0530

    Update push.sh

--- .buildkite/push.sh
+++ .buildkite/push.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 if [ $(buildkite-agent meta-data get approval) == "yes" ]; then
-         cat <<- YAML | buildkite-agent pipeline upload
-         steps:
-           - label: "docker: Push Docker Imag"
-             command: 
-             - 'docker login -u dineshcode97 -p $PASS'
-             - 'docker push $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG'
-      YAML
+         docker login -u dineshcode97 -p $PASS
+         docker push $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 fi
