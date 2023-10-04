bf53bef2407560581dba76d9f9fbc86262598c77
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2023-10-04 13:16:04 +0530

    Update push.sh

--- .buildkite/push.sh
+++ .buildkite/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ $(buildkite-agent meta-data get approval) == "yes" ]; then
-         docker login -u dineshcode97 -p $PASS
+         docker login -u dineshcode97 -p $DPASS
          docker push $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 fi
